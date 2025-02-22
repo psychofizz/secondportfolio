a8fe077d417e7721f8bb0fc26459adfe77a266bd
psychofizz <psychofizz@pm.me>
2025-02-22 09:33:05 -0600

    the proof of concept

---
+++ src/content.config.ts
@@ -0,0 +1,30 @@
+import { glob } from "astro/loaders";
+import { z, defineCollection } from "astro:content";
+
+const blog = defineCollection({
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/blog" }),
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      author: z.string(),
+      image: z.object({
+        url: z.string(),
+        alt: z.string()
+      }),
+      tags: z.array(z.string())
+    })
+});
+
+const projects = defineCollection({
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/projects" }),
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      image: z.object({
+        url: z.string(),
+      }),
+    })
+});
+
+export const collections = { blog, projects };
---
+++ src/interfaces/interface.ts
@@ -0,0 +1,22 @@
+export interface ProjectCard {
+    title: string,
+    description: string,
+    image: string, 
+    link: string
+}
+
+export interface Skill {
+    name: string;
+    logo: string;
+}
+
+export interface Link {
+    href: string;
+    label: string;
+}
+
+export interface Hero {
+    title: string;
+    description: string;
+    image: string;
+}
